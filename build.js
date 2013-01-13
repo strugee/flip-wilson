fdc201aad52ffec8c0900d63b2786ed6fd6f5526
Alex Jordan <alexander3223098@gmail.com>
2013-01-13 12:24:00 -0800

    more build work (still WIP)

--- build.js
+++ build.js
@@ -3,9 +3,13 @@ var mustache = require("mustache");
 
 console.log("loaded mustache, compiling");
 
-fs.readFile("text.json", function(err, data) {
-    if (err) {throw err;}
-    fs.writeFile("build/index.html", mustache.render(data), function (err) {
-        if (err) {throw err;}
-    });
+fs.writeFile("build/index.html", mustache.render("text.json"), function(err) {
+	if (err) throw err;
 });
+/*fs.readFile("text.json", function(err, data) {
+    if (err) throw err;
+    console.log("read file, now compiling");
+    fs.writeFile("build/index.html", mustache.render(data), function (err) {
+        if (err) throw err;
+    });
+});*/
